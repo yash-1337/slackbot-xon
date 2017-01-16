c8840d16211304fe7e26d9c6e50c831723c18551
Yash Patel <Yash Patel>
2017-01-15 20:33:40 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Team-Bot:</h1>
+<p> Team-Bot is used to create teams in Slack based on who is available to join.</p>
+<h2>Getting Started:</h2>
+<p> First fork this repository, then go to http://heroku.com and create an account and create a new app. Go to deploy and click on github and click connect and login to your account and connect to your team-bot repo. Then go to settings and 2 config vars. Name the first one "MONGO_URI" and set it to your mongolab database url. Name the second one and set it to the api token of a bot you create on Slack. Go back to deploy and click deploy. PROFIT!</p>
---
+++ app.js
@@ -0,0 +1,67 @@
+var Botkit = require('./node_modules/botkit/lib/Botkit.js');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.status(200).send('Hello world!');
+});
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
+
+var alive;
+app.post('/keepalive', function (req, res, next) {
+    var http = require("http");
+    alive = setInterval(function () {
+        http.get("http://team-bot.herokuapp.com/keepalive");
+    }, 299990);
+
+
+});
+app.post('/shutdown', function (req, res, next) {
+    var http = require("http");
+    var userName = req.body.user_name;
+    clearInterval(alive);
+    var botPayload = {
+        text: "Jarvis will shutdown after a while."
+    };
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayload);
+    } else {
+        return res.status(200).end();
+    }
+});
+
+var controller = Botkit.slackbot({
+    debug: false
+});
+
+controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    bot.reply(message, "Hello.");
+});
+
+controller.hears(['Introduce yourself'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    bot.reply(message, "I am J.A.R.V.I.S. I was created by Yash Patel.");
+});
+
+controller.hears(['How are you?'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    bot.reply(message, "I\'m fine, thanks for asking!");
+});
+
+controller.hears(['How was your day?'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    bot.reply(message, "It was great! What about you?");
+});
+
+controller.hears(['My day was', "It was"], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    bot.reply(message, "That\'s cool.");
+});
